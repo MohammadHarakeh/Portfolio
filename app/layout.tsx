5e643a0171d164a1f80b7bb2e36af0829df0edfa
MohammadHarakeh <mohammad.d.harakeh@gmail.com>
2026-02-17 13:56:28 +0200

    added speedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import './globals.css'
 import { ThemeProvider } from '@/components/providers/ThemeProvider'
 import { InitialLoading } from "@/components/InitialLoading";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${inter.className} ${inter.variable}`}>
         <InitialLoading />
         <ThemeProvider>{children}</ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
